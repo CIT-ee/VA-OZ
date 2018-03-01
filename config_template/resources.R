cfa4d59919d2256dbcf707732a643f0aa1c22eb3
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2018-03-01 20:04:54 +0000

    dont track raw config dir; add config template

---
+++ config_template/resources.R
@@ -0,0 +1,5 @@
+path_to <- list(
+                clean=file.path(getwd(), 'data', 'clean'),
+                interim=file.path(getwd(), 'data', 'interim'),
+                processed=file.path(getwd(), 'data', 'processed')
+            )
