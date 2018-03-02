48bde7643498b3aa66ea9fe59d59711bccbc57ef
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2018-03-02 01:02:10 +0000

    add func to build incentives mask; init dim builder code

--- src/data/dimension_utils.R
+++ src/data/dimension_utils.R
@@ -5,3 +5,12 @@ add_comm_banks_data <- function(source.dat, tract.dat){
     left_join(source.dat, by=c('county' = 'fips')) %>%
     rename(num_of_commercial_banks=number_of_establishments)
 }
+
+add_incentives_mask <- function(source.dat, tract.dat, srcField){
+  tract.dat %>% 
+    select(geoid) %>% 
+    left_join(source.dat, by = c('geoid' = 'geoid_census_tract')) %>%
+    group_by(geoid) %>% 
+    summarize(is_inzone = n_distinct(!!sym(srcField), na.rm = TRUE)) %>%
+    select(-geoid)
+}
