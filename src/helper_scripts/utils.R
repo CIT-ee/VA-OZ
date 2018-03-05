d41171ee2cf99bbe701d324b4ff6e4d083c392b9
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2018-03-05 16:59:45 +0000

    add: methods to locally write and read data files

--- src/helper_scripts/utils.R
+++ src/helper_scripts/utils.R
@@ -1,3 +1,16 @@
+save_dataframe <- function(dat, path_to_table){
+  path_to_data_dir <- dirname(path_to_table)
+  assert_that(is.dir(path_to_data_dir))
+  write.csv(dat, path_to_table, row.names = FALSE)
+}
+
+load_dataframe <- function(table_name, dim_name){
+  path_to_data_frame <- file.path(path_to[['raw']], dim_name, 
+                                  paste0(table_name, '.csv'))
+  assert_that(file.exists(path_to_data_frame))
+  read.csv(path_to_data_frame, check.names = FALSE)
+}
+
 add_tract_data <- function(source_dat, tract_dat){
   source_dat$fips <- as.character(source_dat$fips)
   left_join(source_dat, tract_dat, by=c('fips' = 'county'))  %>%
