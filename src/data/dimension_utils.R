eed08c620ba37f6d282da42d67f10e0cce9cbab0
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2018-03-05 17:00:40 +0000

    add: code to build the dimensions; update method of fetching data

--- src/data/dimension_utils.R
+++ src/data/dimension_utils.R
@@ -1,3 +1,18 @@
+download_dimension_data <- function(table_names){
+  dim_names <- c(names(table_names))
+  for(dim_idx in seq(length(table_names))){
+    dim_name = dim_names[dim_idx]
+    for(table_name in table_names[[dim_idx]]){
+      path_to_data_frame <- file.path(path_to[['raw']], dim_name, 
+                                      paste0(table_name, '.csv'))
+      if(!file.exists(path_to_data_frame)){
+        sprintf('Saving %s to %s', table_name, path_to_data_frame)
+        save_dataframe(get_dataworld_dat(table_name), path_to_data_frame)
+      }
+    }
+  }
+}
+
 add_county_data <- function(source_dat, tract_dat){
   source_dat$fips <- as.character(source_dat$fips)
 
